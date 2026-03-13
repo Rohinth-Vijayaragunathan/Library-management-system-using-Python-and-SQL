b212380ef87b3cc32f336edf6ec2d44f79155e5f
Rohinth V <rohinth2303@gmail.com>
2026-03-13 18:12:23 +0530

    Update user_auth.py

--- user/user_auth.py
+++ user/user_auth.py
@@ -4,6 +4,15 @@ from user.user_operations import user_menu
 connection = get_connection()
 cursor = connection.cursor()
 
+def register_user():
+    username = input("enter the username: ")
+    password = input("enter the password: ")
+
+    sql = "INSERT INTO User(username, password) values (%s, %s)"
+    cursor.execute(sql , (username, password))
+    connection.commit()
+    print("user created successfully")
+
 def user_login():
 
     u = input("Enter username: ")
@@ -21,3 +30,4 @@ def user_login():
         user_menu(user[0])
     else:
         print("Invalid login")
+
