bcd47aea9daf45f7c519f4e6d9716e42718e4f80
Rohinth V <rohinth2303@gmail.com>
2026-03-13 18:10:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-from admin.admin_auth import admin_login
-from user.user_auth import user_login
+from admin.admin_auth import create_admin, admin_login
+from user.user_auth import user_login, register_user
 
 while True:
 
-    print("\n1.Admin Login")
-    print("2.User Login")
-    print("3.Exit")
+    print("\n1.create admin")
+    print("2.Admin Login")
+    print("3.User Login")
+    print("4.Register user")
+    print("5.Exit")
 
-    choice = int(input("Enter choice: "))
+    try:
+        choice = int(input("Enter choice: "))
+    except ValueError:
+        print("Enter a valid number")
+        continue
 
     if choice == 1:
+        create_admin()
+
+    if choice == 2:
         admin_login()
 
-    elif choice == 2:
+    elif choice == 3:
         user_login()
 
-    else:
+    elif choice == 4:
+        register_user()
+
+    elif choice == 5:
         print("Exiting program")
         break
